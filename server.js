f88d87fb42a7587df1097b675c6e3edaa0992f0a
khanhcoluy <49581756+khanhcoluy@users.noreply.github.com>
2021-07-18 23:44:43 +0700

    remove gitignore files

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
-const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { equal } = require('assert');
 
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
@@ -14,33 +12,31 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(cors());
-
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
-	app.get('*', function (req, res) {
-		res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-	});
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
-app.listen(port, (error) => {
-	if (error) throw error;
-	console.log('Server runner on port' + port);
+app.listen(port, error => {
+  if (error) throw error;
+  console.log('Server running on port ' + port);
 });
 
 app.post('/payment', (req, res) => {
-	const body = {
-		source: req.body.token.id,
-		amount: req.body.amount,
-		currency: 'usd'
-	};
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
-    if(stripeErr) {
+    if (stripeErr) {
       res.status(500).send({ error: stripeErr });
     } else {
       res.status(200).send({ success: stripeRes });
     }
-  })
+  });
 });
