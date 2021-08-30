10a44e941fb5c8114314b0dc9a49e72b9eec12d3
khanhcoluy <49581756+khanhcoluy@users.noreply.github.com>
2021-08-30 22:05:39 +0700

    updating cart sagas to create cart based on user account

--- client/src/redux/cart/cart.sagas.js
+++ client/src/redux/cart/cart.sagas.js
@@ -1,16 +1,56 @@
-import { takeLatest, call, all, put } from '@redux-saga/core/effects';
+import { takeLatest, call, all, put, select } from '@redux-saga/core/effects';
+import { getUserCartRef } from '../../firebase/firebase.utils';
+import { selectCartItems } from './cart.selectors';
+import { selectCurrentUser } from '../user/user.selectors';
 
 import UserActionTypes from '../user/user.types';
-import { clearCart } from './cart.actions';
+import CartActionTypes from './cart.types';
+
+import { clearCart, setCartFromFirebase } from './cart.actions';
 
 export function* clearCartOnSignOut() {
-	yield put(clearCart());
+  yield put(clearCart());
 }
 
+export function* updateCartInFirebase() {
+  const currentUser = yield select(selectCurrentUser);
+  if (currentUser) {
+    try {
+      const cartRef = yield getUserCartRef(currentUser.id);
+      const cartItems = yield select(selectCartItems);
+      yield cartRef.update({ cartItems });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+export function* checkCartFromFirebase({ payload: user }) {
+  const cartRef = yield getUserCartRef(user.id);
+  const cartSnapshot = yield cartRef.get();
+  yield put(setCartFromFirebase(cartSnapshot.data().cartItems));
+}
+
+
 export function* onSignOutSuccess() {
-	yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartOnSignOut);
+  yield takeLatest(UserActionTypes.SIGN_OUT_SUCCESS, clearCartOnSignOut);
+}
+
+export function* onUserSignIn() {
+  yield takeLatest(UserActionTypes.SIGN_IN_SUCCESS, checkCartFromFirebase);
+}
+
+export function* onCartChange() {
+  yield takeLatest(
+    [
+      CartActionTypes.ADD_ITEM,
+      CartActionTypes.REMOVE_ITEM,
+      CartActionTypes.CLEAR_ITEM_FROM_CART
+    ],
+    updateCartInFirebase
+  );
 }
 
 export function* cartSagas() {
-	yield all([call(onSignOutSuccess)]);
+  yield all([call(onSignOutSuccess), call(onCartChange), call(onUserSignIn)]);
 }
--- client/src/redux/cart/cart.types.js
+++ client/src/redux/cart/cart.types.js
@@ -3,7 +3,8 @@ const CartActionTypes = {
     ADD_ITEM: 'ADD_ITEM',
     REMOVE_ITEM: 'REMOVE_ITEM',
     CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART',
-    CLEAR_CART: 'CLEAR_CART' 
-}
+    CLEAR_CART: 'CLEAR_CART',
+    SET_CART_FROM_FIREBASE: 'SET_CART_FROM_FIREBASE'
+};
 
 export default CartActionTypes;
